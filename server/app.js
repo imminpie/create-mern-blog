d09945db5d8daa6a9595942cf83e08fc34e0ba4a
imminpie <mjkang0715@gmail.com>
2023-11-10 20:15:50 +0900

    feat: MongoDB 연결 설정

---
+++ server/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(morgan('common'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect(process.env.MONGO_URL);
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+});
